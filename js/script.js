81d97a8c4aed6e194115ee4f9c45e76f2c0c88c8
nsheludkov <nsheludkov@ya.ru>
2023-05-01 00:28:12 +0300

    feat: keys symbols writes to textarea

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ import keys from './keys.js';
 
 let lang = 'eng';
 let caps = false;
-
+let leftShift = false;
+let rightShift = false;
+let cursor = 0;
 //
 function createAndFillRow(keyboard, arrayOfElements) {
   const currRow = document.createElement('div');
@@ -93,10 +95,79 @@ function updateKeys() {
 
 updateKeys();
 
-function returnSymbolByKeycode(code) {
-
+function isUpperCase() {
+  let isSymbolUpperCase = false;
+  if (caps === true) {
+    isSymbolUpperCase = true;
+    if (leftShift === true || rightShift === true) {
+      isSymbolUpperCase = false;
+    }
+  } else if (leftShift === true || rightShift === true) {
+    isSymbolUpperCase = true;
+  }
+  return isSymbolUpperCase;
 }
 
-window.addEventListener('keydown', (event) => {
-  textarea.value += returnSymbolByKeycode(event.code);
+function returnSymbolByKeycode(code) {
+  let symb = '';
+
+  const key = keys.find((item) => item.name === code);
+
+  if (lang === 'rus') {
+    if (isUpperCase() === true) {
+      symb = key.shiftRus;
+    } else {
+      symb = key.rus;
+    }
+  } else if (isUpperCase() === true) {
+    symb = key.shiftEng;
+  } else {
+    symb = key.eng;
+  }
+  return symb;
+}
+
+window.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  cursor = textarea.selectionStart;
+  const oldTextarea = textarea.value;
+  let newTextarea = '';
+  const keySymbol = returnSymbolByKeycode(e.code);
+  if (keySymbol.indexOf('Alt') >= 0 || keySymbol.indexOf('Ctrl') >= 0 || keySymbol.indexOf('Shift') >= 0
+   || keySymbol.indexOf('Win') >= 0 || keySymbol.indexOf('Caps') >= 0) {
+    return;
+  }
+  if (keySymbol === 'Enter') {
+    const enter = '\n';
+    newTextarea = `${oldTextarea.slice(0, cursor)}${enter}${oldTextarea.slice(cursor)}`;
+    textarea.value = newTextarea;
+    cursor += 1;
+    textarea.setSelectionRange(cursor, cursor);
+    textarea.focus();
+  } else if (keySymbol === 'Tab') {
+    const tab = '    ';
+    newTextarea = `${oldTextarea.slice(0, cursor)}${tab}${oldTextarea.slice(cursor)}`;
+    textarea.value = newTextarea;
+    textarea.setSelectionRange(cursor, cursor);
+    textarea.focus();
+  } else if (e.code === 'Backspace') {
+    if (cursor !== 0) {
+      newTextarea = `${oldTextarea.slice(0, cursor - 1)}${oldTextarea.slice(cursor)}`;
+    }
+    textarea.value = newTextarea;
+    cursor -= 1;
+    textarea.setSelectionRange(cursor, cursor);
+    textarea.focus();
+  } else if (e.code === 'Delete') {
+    newTextarea = `${oldTextarea.slice(0, cursor)}${oldTextarea.slice(cursor + 1)}`;
+    textarea.value = newTextarea;
+    textarea.setSelectionRange(cursor, cursor);
+    textarea.focus();
+  } else {
+    newTextarea = `${oldTextarea.slice(0, cursor)}${keySymbol}${oldTextarea.slice(cursor)}`;
+    textarea.value = newTextarea;
+    cursor += 1;
+    textarea.setSelectionRange(cursor, cursor);
+    textarea.focus();
+  }
 });
